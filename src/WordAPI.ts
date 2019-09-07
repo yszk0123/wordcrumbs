f6cd4334f998b1f275ddd8495bf30e760bf1dc13
Yuji Suzuki <y0suzuki0123@gmail.com>
2019-09-07 13:09:37 +0900

    Extract TokenAPI

---
+++ src/WordAPI.ts
@@ -0,0 +1,83 @@
+import { JSDOM } from 'jsdom';
+import { IpadicFeatures } from 'kuromoji';
+import { tryToFetchBodyFromCacheOr } from './Cache';
+import { debug } from './debug';
+import { createErr, createOk, isErr, Result } from './Result';
+import { Tokenizer, TokenPos } from './Tokenizer';
+
+export enum WordType {
+  DEFAULT,
+  HIGHLIGHT,
+}
+
+export type Word =
+  | { type: WordType.DEFAULT; value: string }
+  | { type: WordType.HIGHLIGHT; value: string };
+
+export class WordAPI {
+  constructor(private readonly tokenizer: Tokenizer) {}
+
+  async fetchFromURL(url: string): Promise<Result<Word[], unknown>> {
+    return fetchTokensFromURL(url, this.tokenizer);
+  }
+}
+
+async function fetchTokensFromURL(
+  url: string,
+  tokenizer: Tokenizer,
+): Promise<Result<Word[], unknown>> {
+  const windowResult = await getWindowFromURL(url);
+  if (isErr(windowResult)) {
+    return createErr(windowResult.val);
+  }
+
+  const { document } = windowResult.val;
+  const content = document.querySelector('#content')!;
+  const text = content.textContent || '';
+  const tokens = tokenizer.tokenize(text);
+  const words = tokens
+    .filter(token => token.pos === TokenPos.NOUN)
+    .map(getWordFromToken);
+  return createOk(words);
+}
+
+function getWordFromToken(token: IpadicFeatures): Word {
+  switch (token.pos) {
+    case TokenPos.NOUN:
+      return { type: WordType.HIGHLIGHT, value: token.basic_form };
+    default:
+      return { type: WordType.DEFAULT, value: token.basic_form };
+  }
+}
+
+async function getWindowFromURL(url: string): Promise<Result<Window, unknown>> {
+  const bodyResult = await getBodyFromURL(url);
+  if (isErr(bodyResult)) {
+    return bodyResult;
+  }
+
+  try {
+    const dom = new JSDOM(bodyResult.val);
+    return createOk(dom.window);
+  } catch (error) {
+    debug(`Failed to parse html`);
+    return createErr(error);
+  }
+}
+
+async function getBodyFromURL(url: string): Promise<Result<string, unknown>> {
+  return tryToFetchBodyFromCacheOr(url, async () => {
+    const response = await fetch(url);
+    if (response.status !== 200) {
+      return createErr(`Failed to fetch "${url}"`);
+    }
+
+    try {
+      const body = await response.text();
+      return createOk(body);
+    } catch (error) {
+      debug(`Failed to parse body "${url}"`);
+      return createErr(error);
+    }
+  });
+}
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,9 @@
 import 'isomorphic-fetch';
-import { JSDOM } from 'jsdom';
-import { IpadicFeatures } from 'kuromoji';
 import makeDir from 'make-dir';
-import { tryToFetchBodyFromCacheOr } from './Cache';
 import { CACHE_DIR } from './constants';
-import { debug } from './debug';
-import { createErr, createOk, isErr, Result } from './Result';
-import { getTokenizer, Tokenizer, TokenPos } from './Tokenizer';
+import { isErr } from './Result';
+import { getTokenizer } from './Tokenizer';
+import { WordAPI } from './WordAPI';
 
 const URL =
   'https://ja.wikipedia.org/wiki/%E3%83%8F%E3%83%B3%E3%83%90%E3%83%BC%E3%82%AC%E3%83%BC%E3%83%9C%E3%82%BF%E3%83%B3';
@@ -23,7 +20,9 @@ async function main() {
     return tokenizerResult;
   }
 
-  const result = await doProcess(tokenizerResult.val);
+  const wordAPI = new WordAPI(tokenizerResult.val);
+
+  const result = await wordAPI.fetchFromURL(URL);
   if (isErr(result)) {
     console.error(result.val);
     return;
@@ -32,77 +31,6 @@ async function main() {
   const words = result.val;
   console.log('========== Words ==========');
   console.log(words.join('\n'));
-}
-
-async function doProcess(
-  tokenizer: Tokenizer,
-): Promise<Result<Word[], unknown>> {
-  const url = URL;
-  const windowResult = await getWindowFromURL(url);
-  if (isErr(windowResult)) {
-    return createErr(windowResult.val);
-  }
-
-  const { document } = windowResult.val;
-  const content = document.querySelector('#content')!;
-  const text = content.textContent || '';
-
-  const tokens = tokenizer.tokenize(text);
-  const words = tokens
-    .filter(token => token.pos === TokenPos.NOUN)
-    .map(getWordFromToken);
-
-  return createOk(words);
-}
-
-enum WordType {
-  DEFAULT,
-  HIGHLIGHT,
-}
-
-type Word =
-  | { type: WordType.DEFAULT; value: string }
-  | { type: WordType.HIGHLIGHT; value: string };
-
-function getWordFromToken(token: IpadicFeatures): Word {
-  switch (token.pos) {
-    case TokenPos.NOUN:
-      return { type: WordType.HIGHLIGHT, value: token.basic_form };
-    default:
-      return { type: WordType.DEFAULT, value: token.basic_form };
-  }
-}
-
-async function getWindowFromURL(url: string): Promise<Result<Window, unknown>> {
-  const bodyResult = await getBodyFromURL(url);
-  if (isErr(bodyResult)) {
-    return bodyResult;
-  }
-
-  try {
-    const dom = new JSDOM(bodyResult.val);
-    return createOk(dom.window);
-  } catch (error) {
-    debug(`Failed to parse html`);
-    return createErr(error);
-  }
-}
-
-async function getBodyFromURL(url: string): Promise<Result<string, unknown>> {
-  return tryToFetchBodyFromCacheOr(url, async () => {
-    const response = await fetch(url);
-    if (response.status !== 200) {
-      return createErr(`Failed to fetch "${url}"`);
-    }
-
-    try {
-      const body = await response.text();
-      return createOk(body);
-    } catch (error) {
-      debug(`Failed to parse body "${url}"`);
-      return createErr(error);
-    }
-  });
 }
 
 main()
